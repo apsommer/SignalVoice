d78f117d07b89c676729682095f55fb47009ffe5
Drew Sommer <drew@sommerengineering.com>
2026-05-09 15:39:30 +0100

    feat: Enable github login provider

--- app/src/main/java/com/sommerengineering/signalvoice/login/GitHubAuthenticator.kt
+++ app/src/main/java/com/sommerengineering/signalvoice/login/GitHubAuthenticator.kt
@@ -1,37 +1,42 @@
 package com.sommerengineering.signalvoice.login
 
 import android.content.Context
+import androidx.activity.ComponentActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.OAuthProvider
 import com.google.firebase.auth.auth
-import com.sommerengineering.signalvoice.MainActivity
 import com.sommerengineering.signalvoice.uitls.gitHubProviderId
 import com.sommerengineering.signalvoice.uitls.logException
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class GitHubAuthenticator @Inject constructor() {
+
+    private val provider =
+        OAuthProvider.newBuilder(gitHubProviderId).build()
 
     suspend fun signIn(
         context: Context
     ): Boolean {
 
-        try {
+        return try {
 
             // launches web browser and backgrounds app
             Firebase.auth
                 .startActivityForSignInWithProvider(
-                    context as MainActivity,
-                    OAuthProvider.newBuilder(gitHubProviderId).build()
+                    context as ComponentActivity,
+                    provider
                 )
                 .await()
 
-            return true
+            true
 
         } catch (e: Exception) {
-            logException(e)
-        }
 
-        return false
+            logException(e)
+            false
+        }
     }
 }
