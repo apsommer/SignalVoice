d44ab43d6a288a7205d0da2d29442289807f6e2e
Drew Sommer <drew@sommerengineering.com>
2026-05-22 21:42:22 +0100

    refactor: Remove unused code

--- app/src/main/java/com/sommerengineering/signalvoice/message/MessageItemStyle.kt
+++ app/src/main/java/com/sommerengineering/signalvoice/message/MessageItemStyle.kt
@@ -1,8 +1,6 @@
 package com.sommerengineering.signalvoice.message
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import com.sommerengineering.signalvoice.source.MessageOrigin
 
 data class MessageItemStyle(
     val primary: Color,
@@ -12,12 +10,3 @@ data class MessageItemStyle(
     val iconRes: Int?,
     val iconText: String? = null
 )
-
-@Composable // todo doesn't need to be composable, raw hex palletes
-fun resolveMessageStyle(
-    origin: MessageOrigin
-) = when (origin) {
-
-    is MessageOrigin.BroadcastStream -> origin.asset.style
-    is MessageOrigin.UserSignal -> origin.source.style
-}
